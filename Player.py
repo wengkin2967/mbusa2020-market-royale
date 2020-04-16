a0a8f125372e9c90fcd58a94050a95717db1d008
Weng Kin Lee <wengkin2967@gmail.com>
2020-04-16 15:18:03 +1000

    Created player.py and gitignore file

---
+++ Player.py
@@ -0,0 +1,11 @@
+# Player class for the assignment
+from BasePlayer import BasePlayer
+
+class Player(BasePlayer):
+    pass;
+
+
+player =  Player()
+print(player)
+player.set_gold(10000.0)
+print(player.gold)
