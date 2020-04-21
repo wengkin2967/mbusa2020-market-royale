dacb8cf5efc6e80571f6637a18215ecc1c46a446
Weng Kin Lee <wengkin2967@gmail.com>
2020-04-21 18:47:44 +1000

    update base files, create player file

--- Player.py
+++ Player.py
@@ -1,13 +1,13 @@
-# Player class for the assignment
+# Player class for assignment
 from BasePlayer import BasePlayer
+import Command
+
+ 
 
 class Player(BasePlayer):
-    pass;
 
-
-player =  Player()
-print(player)
-player.set_gold(10000.0)
-print(player.gold)
-
-print("Hello")
+    def __init__(self):
+        pass
+    
+    def take_turn(self, loc, this_market, info, black_markets, grey_markets):
+        return (Command.RESEARCH, None)
