236152fdc05118c4c3381d5e9eb5881fda033551
Weng Kin Lee <wengkin2967@gmail.com>
2020-04-23 18:01:44 +1000

    model_11
    Modularise a fuck ton, fixed minor logic issues here and there

--- Player.py
+++ Player.py
@@ -51,49 +51,50 @@ class Player(BasePlayer):
         if(location not in self.researched_markets):
             self.researched_markets.append(location)
             return(Command.RESEARCH,None)
-        # If market researched, should aim to buy for goals
+        # If market researched, should aim to buy for goals or sell
         else:
             # Add market info to tracker
             self.market_visited_info[location] = prices
 
             # Arrange goal based on cheapest to acquire
-            priority_goals = sorted(not_achieved_goals, key= lambda x: prices.get(x,(999999,0))[0] * abs(self.goal[x] - self.inventory_tracker.get(x,(0,0))[0]))
+            priority_goals = sorted(not_achieved_goals, key= lambda x: prices.get(x,(999999,0))[0] * 
+                                    abs(self.goal[x] - self.inventory_tracker.get(x,(0,0))[0]))
             
-            # Selling if excess
-            for item in [goal for goal in self.goal if goal not in not_achieved_goals]:
-                if (self.inventory_tracker.get(item,(0,0))[0] > self.goal[item]):
+            # Selling if excess and can make a profit
+            for item in self.goals_achieved():
+                if (self.excess_item(item) and 
+                    self.got_profit(prices.get(item,(0,0))[0],item)):
 
                     item_amount = self.inventory_tracker.get(item,(0,0))[0] - self.goal[item]
-                    self.inventory_tracker[item] = (self.inventory_tracker[item][0] - item_amount,
-                            self.inventory_tracker[item][1])
-                    return (Command.SELL, (item,item_amount))
+                    return self.sell_item(item,item_amount,prices.get(item,(0,0))[0])
 
             for item in priority_goals:
+                
                 # Sell if cannot meet goal
-                if (self.turn_tracker >= 46 and self.inventory_tracker.get(item,(9999,9999))[0] < self.goal[item]):
+                if (self.turn_tracker >= 46 and 
+                    self.inventory_tracker.get(item,(9999,9999))[0] < self.goal[item]):
 
                     item_amount = self.inventory_tracker[item][0]
-                    self.inventory_tracker[item] = (self.inventory_tracker[item][0] - item_amount,
-                                                    9999)
-                    return (Command.SELL, (item,item_amount))
+                    return self.sell_item(item,item_amount,prices.get(item,(0,0))[0])
 
                 # Checking if player info has valuable information
                 if(item in prices.keys() and self.turn_tracker < 40):
                     for market in self.player_info.keys():
-                        if(self.map.is_road(location,market) and 
+                        if (self.map.is_road(location,market) and 
                         self.player_info[market][item] < prices[item][0]
-                        and min(self.goal[item], prices[item][1]) * prices[item][0]  -  
-                            min(self.goal[item],prices[item][1]) *  self.player_info[market][item] >= 1000):
+                        and self.worth_moving(item, prices[item], self.player_info[market][item],1000)):
+
                             return (Command.MOVE_TO, market)
                 
                 # For each item in price, check that there is enough
                 # stock, enough gold, less than 10k and that goal
                 # hasn't been met yet
-                if(item in prices.keys() and prices[item][1] > 0 and self.goal_is_worth_and_enough_money(prices,item) and 
+                if(item in prices.keys() and prices[item][1] > 0 and 
+                    self.goal_is_worth_and_enough_money(prices,item) and 
                     not self.goal_met(item)):
+
                     amount = min(self.goal[item], prices[item][1])
-                    self.inventory_tracker[item] =  (self.inventory_tracker.get(item,(0,0))[0] + amount, prices[item][0])
-                    self.gold -= amount * prices[item][0]
+                    self.buy_item(item,amount,prices[item][0])
                     return (Command.BUY,(item, amount))
         
         # Moving if area is grey or black
@@ -152,6 +153,7 @@ class Player(BasePlayer):
         # Populate the dist dictionary
         for node in pos.keys():
             dist[node] = sqrt((map_width/2.0 - pos[node][0])**2 + (map_height/2.0 - pos[node][1])**2)
+            # dist[node] = abs(map_width/2.0 - pos[node][0]) + abs(map_height/2.0 - pos[node][1])
 
         return dist
 
@@ -182,3 +184,53 @@ class Player(BasePlayer):
         Helper to return all goals not yet achieved.
         """
         return [goal for goal in self.goal.keys() if not self.goal_met(goal)]
+
+    def buy_item(self,item,item_amount,price):
+        """
+        Helper to buy an item, item_amount times
+        """
+        self.inventory_tracker[item] =  (self.inventory_tracker.get(item,(0,0))[0] + item_amount, price)
+        self.gold -= item_amount * price
+        return (Command.BUY,(item, item_amount))
+
+    def worth_moving(self, item, item_price_amount, new_price, threshold):
+        """
+        Helper to  check if it is worth moving to the new market to purchase 
+        product for achieving the goal by having a threshold difference
+        """
+
+        final_amount = min(self.goal[item], item_price_amount[1])
+        difference_price = (item_price_amount[0] - new_price)
+
+        return final_amount * difference_price  >= threshold
+
+    #  Selling Logic Functions
+
+    def goals_achieved(self):
+        """
+        Helper to return goals achieved.
+        """
+
+        return [goal for goal in self.goal.keys() if  self.goal_met(goal)]
+
+    def sell_item(self,item,item_amount,price):
+        """
+        Helper to sell item based on item_amount
+        """
+        self.inventory_tracker[item] = (self.inventory_tracker[item][0] - item_amount,
+                                        self.inventory_tracker[item][1])
+        self.gold += price * item_amount
+        return (Command.SELL, (item,item_amount))
+
+    def excess_item(self,item):
+        """
+        Helper to see if inventory has excess of an item with respect to goal
+        """
+        return self.inventory_tracker.get(item,(0,0))[0] > self.goal[item]
+
+    def got_profit(self,price,item):
+        """
+        Helper to determine whether selling item yields profit
+        """
+        price > self.inventory_tracker.get(item,(0,9999))[1]
+
