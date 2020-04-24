f2ae633b994df7ae51f881eb47dee764e8fe0e28
binxingl <63770560+binxingl@users.noreply.github.com>
2020-04-25 00:52:25 +1000

    Include Shortest Path Function

--- Player.py
+++ Player.py
@@ -14,15 +14,10 @@ class Player(BasePlayer):
    # def __init__(self):
     #    super().__init__()
     
-    
-    def take_turn(self, location, prices, info, bm, gm):
-        if self.danger(location,bm,gm) or location in bm or location in gm:
-            return (Command.MOVE_TO, self.move(location))
-        else:
-            return (Command.PASS, None)
-    
-    #returns true if only 1 connecting node is white
     def danger(self, location, bm, gm):
+        """
+        Function that returns true or false if in danger
+        """
         counter = 0
         for node in list(self.map.get_neighbours(location)):
             #white markets
@@ -33,39 +28,49 @@ class Player(BasePlayer):
         else:
             return False
 
-    #returns node that leads to target node
-    def move(self, location):
-        availablenodes = list(self.map.get_neighbours(location))
-        d = defaultdict(int)
-        for node in availablenodes:
-            d[node] = self.dist(node)
-        d = sorted(d.items(), key = lambda k: k[1])
-        return d[0][0]
+    def centrenode(self): 
+        """
+        Finds centrenode based mapwidth and mapheight
+        """
+        targetnodelist = []
+        mapheight = (self.map.map_height)/2
+        mapwidth = (self.map.map_width)/2
+        for node, coordinates in self.map.map_data['node_positions'].items():
+            x, y, circlestatus = coordinates
+            x_abs = abs(mapwidth - x)
+            y_abs = abs(mapheight - y)
+            targetnodelist.append([node, x_abs + y_abs])
+        return sorted (targetnodelist, key = lambda node: node[1])[0][0]
+
+    def shortest_path(self, location):
+        """
+        Finds shortest path between any location and centrenode using BFS
+        """
+        graph = self.map.map_data['node_graph']
+        #keeps track of explored nodes
+        explored = []
+        #keeps track of all paths to be checked
+        queue = [[location]]
         
-    #finds target_node based on map height and map width
-    def centrenode(self):
-       x = []
-       y = []
-       node = []
-       targetmapheight = (self.map.map_height)/2
-       targetmapwidth = (self.map.map_width)/2
-       for i in self.map.map_data['node_positions']:
-           node.append(i)
-           x.append(self.map.map_data['node_positions'][i][0])
-           y.append(self.map.map_data['node_positions'][i][1])
-       coordinates = zip(node,x,y)
-       targetnodelist = []
-       for node,x,y in coordinates:
-           x_abs = abs(targetmapwidth - x)
-           y_abs = abs(targetmapheight - y)
-           targetnodelist.append([node, x_abs+y_abs])
-       return sorted(targetnodelist, key = lambda node: node[1])[0][0]
-   
-    #function to calculate distance between target node and current loc
-    def dist(self, location):
-       x_target = self.map.map_data['node_positions'][self.centrenode()][0]
-       y_target = self.map.map_data['node_positions'][self.centrenode()][1]
-       x_loc = self.map.map_data['node_positions'][location][0]
-       y_loc = self.map.map_data['node_positions'][location][1]
-       dist = sqrt(((x_target-x_loc)**2)+((y_target-y_loc)**2))
-       return dist
+        # return path if location is centrenode
+        if location == self.centrenode():
+            return True
+        
+        # loops until all possible paths are checked
+        while queue:
+            #pop first path from queue
+            path = queue.pop(0)
+            #gets last node from the path
+            node = path[-1]
+            if node not in explored:
+                neighbours = graph[node]
+                #go through all neighbournodes, construct new path
+                #and push to queue
+                for neighbour in neighbours:
+                    new_path  = list(path)
+                    new_path.append(neighbour)
+                    queue.append(new_path)
+                    #return path if neighbour is centrenode
+                    if neighbour == self.centrenode():
+                        return new_path
+                explored.append(node)
