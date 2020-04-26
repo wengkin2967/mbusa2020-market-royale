a34ce03e91aa5954cf3a594991212678568b8e2a
JiaanL <53369926+JiaanL@users.noreply.github.com>
2020-04-26 15:49:56 +1000

    change test

--- upload.py
+++ upload.py
@@ -25,8 +25,8 @@ if len(sys.argv) > 1:
     cmd = sys.argv[1]
     name = sys.argv[2]
 else:
-    cmd = "DEL"
-    name = ">_< Artificial_Idiot 2"
+    cmd = "ADD"
+    name = ">_< Artificial_Idiot"
 
 request = {
     "cmd": cmd,
